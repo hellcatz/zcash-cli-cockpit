c5522dba7f0dd3bc5162203f01341bea7b74a345
hellcatz <kg6ypi@remotehams.com>
2017-06-10 17:39:03 -0700

    Update zcash-cli.js
    Added example configs

--- zcash-cli.js
+++ zcash-cli.js
@@ -13,13 +13,41 @@ Copyright (c) 2016 github.com/hellcatz
 //
 // Config Variables
 //
-var symbol = "ZEN"
-var zcashd = "zend";
-var zcashcli = "zen-cli";
+// Example Zcash Config
+var symbol = "ZEC"
+var zcashd = "zcashd";
+var zcashcli = "zcash-cli";
 var zcash_getinfo_rate = 60000;
-var explorer_transactions_url = "http://node1.zenchain.info:8886/tx/";
-//var explorer_transactions_url = "https://explorer.zcha.in/transactions/";
+var explorer_transactions_url = "https://explorer.zcha.in/transactions/";
+
+// Example Komodo Config
+//var symbol = "KMD"
+//var zcashd = "komodod";
+//var zcashcli = "komodo-cli";
+//var zcash_getinfo_rate = 60000;
 //var explorer_transactions_url = "http://kmd.explorer.supernet.org/tx/";
+
+// Example Zclassic Config
+//var symbol = "ZCL"
+//var zcashd = "zcashd";
+//var zcashcli = "zclassic-cli";
+//var zcash_getinfo_rate = 60000;
+//var explorer_transactions_url = "https://aayanl.tech:3000/insight/tx/";
+
+// Example ZenCash Config
+//var symbol = "ZEN"
+//var zcashd = "zend";
+//var zcashcli = "zen-cli";
+//var zcash_getinfo_rate = 60000;
+//var explorer_transactions_url = "http://node1.zenchain.info:8886/tx/";
+
+
+
+/*
+-----------------------------
+  DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOUR DOING
+-----------------------------
+*/
 
 
 
