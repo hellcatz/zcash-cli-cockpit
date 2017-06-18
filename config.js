84b5aeb5fdf997950cf5ab4d706156065cad1baa
hellcatz <kg6ypi@remotehams.com>
2017-06-17 20:22:42 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+// Example Zcash Config
+var symbol = "ZEC"
+var zcashd = "zcashd";
+var zcashcli = "zcash-cli";
+var zcash_getinfo_rate = 60000;
+var explorer_transactions_url = "https://explorer.zcha.in/transactions/";
+
+// Example Zclassic Config
+//var symbol = "ZCL"
+//var zcashd = "zcashd";
+//var zcashcli = "zclassic-cli";
+//var zcash_getinfo_rate = 60000;
+//var explorer_transactions_url = "https://aayanl.tech:3000/insight/tx/";
+
+// Example ZenCash Config
+//var symbol = "ZEN"
+//var zcashd = "zend";
+//var zcashcli = "zen-cli";
+//var zcash_getinfo_rate = 60000;
+//var explorer_transactions_url = "http://node1.zenchain.info:8886/tx/";
+
+// Example Hush Config
+//var symbol = "HUSH"
+//var zcashd = "hushd";
+//var zcashcli = "hush-cli";
+//var zcash_getinfo_rate = 60000;
+//var explorer_transactions_url = "http://explorer.myhush.org/tx/";
+
+// Example Komodo Config
+//var symbol = "KMD"
+//var zcashd = "komodod";
+//var zcashcli = "komodo-cli";
+//var zcash_getinfo_rate = 60000;
+//var explorer_transactions_url = "http://kmd.explorer.supernet.org/tx/";
